d3ebb1562bff1cac23f251c700d44dd3b7f165f2
Jaime Caicedo <jaime.caicedo@gmail.com>
2016-01-13 12:05:53 -0500

    Test commit. Added a new line
    Signed-off-by: Jaime Caicedo <jaime.caicedo@gmail.com>

--- plotting/dfplots.py
+++ plotting/dfplots.py
@@ -3,6 +3,7 @@
 The SpanSelector is a mouse widget to select a xmin/xmax range and plot the
 detail view of the selected region in the lower axes
 """
+
 import matplotlib as mpl
 import numpy as np
 import matplotlib.pyplot as plt
