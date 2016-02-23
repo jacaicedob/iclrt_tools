d603c79886ea4bbb35c9b2e90179daf3a408e8a1
Jaime Caicedo <jaime.caicedo@gmail.com>
2016-02-23 13:59:14 -0500

    Minimal changes

--- examples/RadarPlots/radar_plots.py
+++ examples/RadarPlots/radar_plots.py
@@ -63,21 +63,78 @@ radar_files = ['./KJAX20150827_233020_V06.gz',
                './KJAX20150828_005151_V06.gz',
                './KJAX20150828_005630_V06.gz']
 
+parent = '/home/jaime/Documents/ResearchTopics/Publications/Lightning Evolution/Storm 07-17-2012/John Data/Radar Data/KJAX'
+os.chdir(parent)
+
+radar_files = ['./KJAX_SDUS52_N0RJAX_201207171803',
+               './KJAX_SDUS52_N0RJAX_201207171808',
+               './KJAX_SDUS52_N0RJAX_201207171812',
+               './KJAX_SDUS52_N0RJAX_201207171817',
+               './KJAX_SDUS52_N0RJAX_201207171822',
+               './KJAX_SDUS52_N0RJAX_201207171827',
+               './KJAX_SDUS52_N0RJAX_201207171831',
+               './KJAX_SDUS52_N0RJAX_201207171836',
+               './KJAX_SDUS52_N0RJAX_201207171841',
+               './KJAX_SDUS52_N0RJAX_201207171845',
+               './KJAX_SDUS52_N0RJAX_201207171850',
+               './KJAX_SDUS52_N0RJAX_201207171855',
+               './KJAX_SDUS52_N0RJAX_201207171900',
+               './KJAX_SDUS52_N0RJAX_201207171904',
+               './KJAX_SDUS52_N0RJAX_201207171909',
+               './KJAX_SDUS52_N0RJAX_201207171914',
+               './KJAX_SDUS52_N0RJAX_201207171918',
+               './KJAX_SDUS52_N0RJAX_201207171923',
+               './KJAX_SDUS52_N0RJAX_201207171928',
+               './KJAX_SDUS52_N0RJAX_201207171932',
+               './KJAX_SDUS52_N0RJAX_201207171937',
+               './KJAX_SDUS52_N0RJAX_201207171942',
+               './KJAX_SDUS52_N0RJAX_201207171947',
+               './KJAX_SDUS52_N0RJAX_201207171951',
+               './KJAX_SDUS52_N0RJAX_201207171956',
+               './KJAX_SDUS52_N0RJAX_201207172001',
+               './KJAX_SDUS52_N0RJAX_201207172005',
+               './KJAX_SDUS52_N0RJAX_201207172010',
+               './KJAX_SDUS52_N0RJAX_201207172015',
+               './KJAX_SDUS52_N0RJAX_201207172020',
+               './KJAX_SDUS52_N0RJAX_201207172024',
+               './KJAX_SDUS52_N0RJAX_201207172029',
+               './KJAX_SDUS52_N0RJAX_201207172034',
+               './KJAX_SDUS52_N0RJAX_201207172039',
+               './KJAX_SDUS52_N0RJAX_201207172044',
+               './KJAX_SDUS52_N0RJAX_201207172048',
+               './KJAX_SDUS52_N0RJAX_201207172053',
+               './KJAX_SDUS52_N0RJAX_201207172058',
+               './KJAX_SDUS52_N0RJAX_201207172102',
+               './KJAX_SDUS52_N0RJAX_201207172107',
+               './KJAX_SDUS52_N0RJAX_201207172111',
+               './KJAX_SDUS52_N0RJAX_201207172116',
+               './KJAX_SDUS52_N0RJAX_201207172121',
+               './KJAX_SDUS52_N0RJAX_201207172126',
+               './KJAX_SDUS52_N0RJAX_201207172130',
+               './KJAX_SDUS52_N0RJAX_201207172135',
+               './KJAX_SDUS52_N0RJAX_201207172140',
+               './KJAX_SDUS52_N0RJAX_201207172144',
+               './KJAX_SDUS52_N0RJAX_201207172149',
+               './KJAX_SDUS52_N0RJAX_201207172154',
+               './KJAX_SDUS52_N0RJAX_201207172159']
+
 for radar_file in radar_files:
     print("Reading radar file: " + radar_file)
     radar_plotter = df.RadarPlotter(radar_file)
 
     print("  - Plotting")
-    # fig, ax = plt.subplots(1, 1)
-    # radar_plotter.plot_ppi('reflectivity', fig=fig, ax=ax)
-    # ax.scatter(0, 0, s=50, c='w')
-    # ax.set_xlim([-20, 20])
-    # ax.set_ylim([-20, 20])
-    # ax.set_xlabel('West - East (km)')
-    # ax.set_ylabel('South - North (km)')
+    fig, ax = plt.subplots(1, 1)
+    radar_plotter.plot_ppi('reflectivity', fig=fig, ax=ax)
+    ax.scatter(0, 0, s=50, c='w')
+    ax.set_xlim([-20, 20])
+    ax.set_ylim([-20, 20])
+    ax.set_xlabel('West - East (km)')
+    ax.set_ylabel('South - North (km)')
     # ax.set_title(radar_file[2:-7])
     # save_file = './PNG/' + radar_file[2:-2] + 'png'
-    # fig.savefig(save_file, dpi=300, format='png')
+    ax.set_title(radar_file[2:])
+    save_file = './PNG/' + radar_file[2:] + '.png'
+    fig.savefig(save_file, dpi=300, format='png')
 
     # fig, ax = plt.subplots(1,3)
     # radar_plotter.plot_pseudo_rhi('reflectivity', azimuth=188.71, fig=fig, ax=ax[0])
@@ -91,5 +148,5 @@ for radar_file in radar_files:
     # ax[2].set_ylim([0, 20])
     # plt.show()
 
-    radar_plotter.plot_ppi_rhi()
-    plt.show()
+    # radar_plotter.plot_ppi_rhi()
+    # plt.show()
