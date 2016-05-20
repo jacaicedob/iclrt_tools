4a5f073f344565cc5f45e73e250c6bd625ec74d3
Jaime Caicedo <jaime.caicedo@gmail.com>
2016-05-20 16:37:52 -0400

    Added the functionality to save and load pickled files.

--- examples/LMA Statistical Analysis/storm_03042016.py
+++ examples/LMA Statistical Analysis/storm_03042016.py
@@ -5,64 +5,121 @@ import datetime
 import iclrt_tools.lma.analysis.storm_analysis as st
 import matplotlib.pyplot as plt
 import seaborn as sns
+import os
+import pickle
 
-# File names for first and second cell of storm
-cell_1_files = ['/home/jaime/Documents/ResearchTopics/Publications/LightningEvolution/Storm-03-04-2016/LMA/ChargeAnalysis_Cell1_0540-exported.csv',
-                '/home/jaime/Documents/ResearchTopics/Publications/LightningEvolution/Storm-03-04-2016/LMA/ChargeAnalysis_Cell1_0550-exported.csv',
-                '/home/jaime/Documents/ResearchTopics/Publications/LightningEvolution/Storm-03-04-2016/LMA/ChargeAnalysis_Cell1_0600-exported.csv',
-                '/home/jaime/Documents/ResearchTopics/Publications/LightningEvolution/Storm-03-04-2016/LMA/ChargeAnalysis_Cell1_0610-exported.csv',
-                '/home/jaime/Documents/ResearchTopics/Publications/LightningEvolution/Storm-03-04-2016/LMA/ChargeAnalysis_Cell1_0620-exported.csv']
+# Operations to be performed on raw data
+sort_flashes_into_cells = False
+get_flash_numbers_from_lma = True
 
-cell_2_files = ['/home/jaime/Documents/ResearchTopics/Publications/LightningEvolution/Storm-03-04-2016/LMA/ChargeAnalysis_Cell2_0600-exported.csv',
-                '/home/jaime/Documents/ResearchTopics/Publications/LightningEvolution/Storm-03-04-2016/LMA/ChargeAnalysis_Cell2_0610-exported.csv',
-                '/home/jaime/Documents/ResearchTopics/Publications/LightningEvolution/Storm-03-04-2016/LMA/ChargeAnalysis_Cell2_0620-exported.csv',
-                '/home/jaime/Documents/ResearchTopics/Publications/LightningEvolution/Storm-03-04-2016/LMA/ChargeAnalysis_Cell2_0630-exported.csv',
-                '/home/jaime/Documents/ResearchTopics/Publications/LightningEvolution/Storm-03-04-2016/LMA/ChargeAnalysis_Cell2_0640-exported.csv',
-                '/home/jaime/Documents/ResearchTopics/Publications/LightningEvolution/Storm-03-04-2016/LMA/ChargeAnalysis_Cell2_0650-exported.csv',
-                '/home/jaime/Documents/ResearchTopics/Publications/LightningEvolution/Storm-03-04-2016/LMA/ChargeAnalysis_Cell2_0700-exported.csv']
+path = '/home/jaime/Documents/ResearchTopics/Publications/'\
+       'LightningEvolution/Storm-03-04-2016/'
+
+# LOAD LMA DATA
+# Raw LMA files
+cell_1_lma_files = [path + 'LMA/ChargeAnalysis_Cell1_0540-exported.csv',
+                     path + 'LMA/ChargeAnalysis_Cell1_0550-exported.csv',
+                     path + 'LMA/ChargeAnalysis_Cell1_0600-exported.csv',
+                     path + 'LMA/ChargeAnalysis_Cell1_0610-exported.csv',
+                     path + 'LMA/ChargeAnalysis_Cell1_0620-exported.csv']
+
+cell_2_lma_files = [path + 'LMA/ChargeAnalysis_Cell2_0600-exported.csv',
+                    path + 'LMA/ChargeAnalysis_Cell2_0610-exported.csv',
+                    path + 'LMA/ChargeAnalysis_Cell2_0620-exported.csv',
+                    path + 'LMA/ChargeAnalysis_Cell2_0630-exported.csv',
+                    path + 'LMA/ChargeAnalysis_Cell2_0640-exported.csv',
+                    path + 'LMA/ChargeAnalysis_Cell2_0650-exported.csv',
+                    path + 'LMA/ChargeAnalysis_Cell2_0700-exported.csv']
 
 dates = ['03/04/2016']
 
-file_name = '/home/jaime/Documents/ResearchTopics/Publications/LightningEvolution/Storm-03-04-2016/Cell1 Analysis 03042016.csv'
+# Pickled files
+cell_1_pickle = path + 'Statistical Analysis/cell_1_lma.p'
+cell_2_pickle = path + 'Statistical Analysis/cell_2_lma.p'
 
-file_name_2 = '/home/jaime/Documents/ResearchTopics/Publications/LightningEvolution/Storm-03-04-2016/Cell2 Analysis 03042016.csv'
+# Load Cell 1 LMA
+if not(os.path.isfile(cell_1_pickle)):
+    storm_lma_1 = st.StormLMA.from_lma_files(cell_1_lma_files, dates)
+else:
+    storm_lma_1 = st.StormLMA()
+    storm_lma_1.from_pickle(cell_1_pickle)
 
-storm_lma_1 = st.StormLMA.from_lma_files(cell_1_files, dates)
-storm_lma_2 = st.StormLMA.from_lma_files(cell_2_files, dates)
+# Load Cell 1 LMA
+if not(os.path.isfile(cell_2_pickle)):
+    storm_lma_2 = st.StormLMA.from_lma_files(cell_2_lma_files, dates)
+else:
+    storm_lma_2 = st.StormLMA()
+    storm_lma_2.from_pickle(cell_2_pickle)
 
-storm_ods_1 = st.StormODS.from_ods_file(file_name)
-storm_ods_2 = st.StormODS.from_ods_file(file_name_2)
-st.Analysis.nice_plots()
+# LOAD .ODS DATA
+# Raw CSV files
+cell_1_ods_file = path + 'Cell1 Analysis 03042016.csv'
+cell_2_ods_file = path + 'Cell2 Analysis 03042016.csv'
 
-# storm_lma_2.plot_charge_region(charge='positive')
-# storm_lma_2.plot_charge_region(charge='negative')
-# storm_lma_2.plot_all_charge_regions(show_plot=True)
+# Pickled files
+cell_1_ods_pickle = path + 'Statistical Analysis/cell_1_ods.p'
+cell_2_ods_pickle = path + 'Statistical Analysis/cell_2_ods.p'
 
-# ics = storm_ods_1.get_flash_type('IC')
-# cgs = storm_ods_1.get_flash_type('-CG')
-ics = storm_ods_2.get_flash_type('IC')
-cgs = storm_ods_2.get_flash_type('-CG')
+# Load Cell 1 ods
+if not(os.path.isfile(cell_1_ods_pickle)):
+    storm_ods_1 = st.StormODS.from_ods_file(cell_1_ods_file)
+else:
+    storm_ods_1 = st.StormODS()
+    storm_ods_1.from_pickle(cell_1_ods_pickle)
 
-# ic_series = ics['Initiation Height (km)']
-# cg_series = cgs['Initiation Height (km)']
-ic_series = ics['Area (km^2)']
-cg_series = cgs['Area (km^2)']
+# Load Cell 2 ods
+if not(os.path.isfile(cell_2_ods_pickle)):
+    storm_ods_2 = st.StormODS.from_ods_file(cell_2_ods_file)
+else:
+    storm_ods_2 = st.StormODS()
+    storm_ods_2.from_pickle(cell_2_ods_pickle)
 
-data_frame = pd.DataFrame({' ICs': ic_series, '-CGs': cg_series})
-fig, ax = plt.subplots(1, 1, figsize=(12, 6))
-data_frame.plot.hist(alpha=0.5, ax=ax)
-# ax.set_title('Histogram of Initiation Heights for ICs and -CGs')
-# ax.set_xlabel('Initiation Height (km)')
-ax.set_title('Histogram of Flash Areas (from Plan View) for ICs and -CGs')
-ax.set_xlabel(r'Flash Area (km$^2$)')
-ax.legend()
-plt.show()
+# Perform operations on raw data (if applicable)
+if get_flash_numbers_from_lma:
+    # Cell 1
+    data = st.StormLMA(storm_lma_1.filter_stations(6, inplace=False))
+    data.filter_chi_squared(5, inplace=True)
+    s = storm_ods_1.get_analyzed_flash_numbers(data, verbose=True)
+    storm_ods_1 = st.StormODS(s)
+
+    # Cell 2
+    data = st.StormLMA(storm_lma_2.filter_stations(6, inplace=False))
+    data.filter_chi_squared(5, inplace=True)
+    s = storm_ods_2.get_analyzed_flash_numbers(data, verbose=True)
+    storm_ods_2 = st.StormODS(s)
 
 
-# storm_ods_1.get_flash_rate(category='IC')
-# storm_ods_1.get_flash_rate(category='CG')
-# storm_lma_1.plot_all_charge_regions()
+# st.Analysis.nice_plots()
 #
-# storm_ods_2.get_flash_rate(category='IC')
-# storm_ods_2.get_flash_rate(category='CG')
-# storm_lma_2.plot_all_charge_regions(show_plot=True)
+# # storm_lma_2.plot_charge_region(charge='positive')
+# # storm_lma_2.plot_charge_region(charge='negative')
+# # storm_lma_2.plot_all_charge_regions(show_plot=True)
+#
+# # ics = storm_ods_1.get_flash_type('IC')
+# # cgs = storm_ods_1.get_flash_type('-CG')
+# ics = storm_ods_2.get_flash_type('IC')
+# cgs = storm_ods_2.get_flash_type('-CG')
+#
+# # ic_series = ics['Initiation Height (km)']
+# # cg_series = cgs['Initiation Height (km)']
+# ic_series = ics['Area (km^2)']
+# cg_series = cgs['Area (km^2)']
+#
+# data_frame = pd.DataFrame({' ICs': ic_series, '-CGs': cg_series})
+# fig, ax = plt.subplots(1, 1, figsize=(12, 6))
+# data_frame.plot.hist(alpha=0.5, ax=ax)
+# # ax.set_title('Histogram of Initiation Heights for ICs and -CGs')
+# # ax.set_xlabel('Initiation Height (km)')
+# ax.set_title('Histogram of Flash Areas (from Plan View) for ICs and -CGs')
+# ax.set_xlabel(r'Flash Area (km$^2$)')
+# ax.legend()
+# plt.show()
+#
+#
+# # storm_ods_1.get_flash_rate(category='IC')
+# # storm_ods_1.get_flash_rate(category='CG')
+# # storm_lma_1.plot_all_charge_regions()
+# #
+# # storm_ods_2.get_flash_rate(category='IC')
+# # storm_ods_2.get_flash_rate(category='CG')
+# # storm_lma_2.plot_all_charge_regions(show_plot=True)
