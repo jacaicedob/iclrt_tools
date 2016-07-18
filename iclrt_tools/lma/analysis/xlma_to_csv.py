f1632ceb39cbabf00d0a72d912f1c67efd8b85b8
Jaime Caicedo <jaime.caicedo@gmail.com>
2016-07-18 14:50:21 -0400

    Made the code to work directly from the .dat.gz files exported from xlma.

--- iclrt_tools/lma/analysis/xlma_to_csv.py
+++ iclrt_tools/lma/analysis/xlma_to_csv.py
@@ -5,25 +5,56 @@ Converts the exported files from the xlma software into .csv files.
 """
 
 import sys
+import gzip
 
 if len(sys.argv) > 1:
     files = sys.argv[1:]
-    print(files)
+
     for file in files:
         lines = ''
 
-        # Read and convert all lines
-        with open(file, 'r') as f:
-            print("Reading File: {0}".format(file))
-            lines += f.readline()
-            for line in f:
-                words = line.split()
-                lines += ','.join(words) + '\n'
+        if 'gz' in file[-3:]:
+            # Read and convert all lines
+            with gzip.open(file, 'rt') as f:
+                print("Reading File: {0}".format(file))
 
-        # Write all lines
-        out = file[:-3] + 'csv'
-        with open(out, 'w') as f:
-            print("Writing File: {0}".format(out))
-            f.write(lines)
+                while not '*** data ***' in f.readline():
+                    pass
+
+                lines += 'time(UT-sec-of-day),lat,lon,alt(m),reduced-chi^2,' \
+                         '#-of-stations-contributed,P(dBW),flash-number,' \
+                         'charge,mask\n'
+
+                for line in f:
+                    words = line.split()
+                    lines += ','.join(words) + '\n'
+
+            # Write all lines
+            out = file[:-6] + 'csv'
+            with open(out, 'w') as f:
+                print("Writing File: {0}".format(out))
+                f.write(lines)
+
+        else:
+            # Read and convert all lines
+            with open(file, 'rt') as f:
+                print("Reading File: {0}".format(file))
+
+                while not '*** data ***' in f.readline():
+                    pass
+
+                lines += 'time(UT-sec-of-day),lat,lon,alt(m),reduced-chi^2,' \
+                         '#-of-stations-contributed,P(dBW),flash-number,' \
+                         'charge,mask\n'
+
+                for line in f:
+                    words = line.split()
+                    lines += ','.join(words) + '\n'
+
+            # Write all lines
+            out = file[:-3] + 'csv'
+            with open(out, 'w') as f:
+                print("Writing File: {0}".format(out))
+                f.write(lines)
 
         print('Done \n\n')
